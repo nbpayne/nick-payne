3932bc5af981b454b11d350d6be2e08a41923653
Nick Payne <nick@insuredbyus.com>
2018-09-16 16:34:39 +1000

    Get gigs from SongKick API and display in footer

--- __js/main.js
+++ __js/main.js
@@ -9,3 +9,24 @@ $('#navbarSupportedContent').on('hide.bs.collapse', function () {
   $('.hometron').removeClass('hidden').addClass('visible');
   $('header').removeClass('darken').addClass('undarken');
 })
+
+// Get Songkick gigs
+$.getJSON(
+  'http://api.songkick.com/api/3.0/artists/mbid:e794939d-318d-47af-b253-2994d09330da/calendar.json?apikey=hRGXicugpn2NSNFF',
+  renderGigs
+)
+
+// Render Songkick gigs
+function renderGigs(data) {
+  var events = data.resultsPage.results.event;
+  if(events.length) {
+    for (var i = events.length - 1; i >= 0; i--) {
+      $('#gigs').append(
+        '<dt>' + moment(events[i].start.date).format('MMM Do, YYYY') + '</dt>' +
+        '<dd><a href="/tour/">' + events[i].displayName + '</a></dd>'
+      );
+    }  
+  } else {
+    $('#gigs').replaceWith('<p>No upcoming dates</p>');
+  }
+}
